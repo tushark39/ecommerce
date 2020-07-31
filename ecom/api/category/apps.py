6d4f7f7ebe9920cce9eb8cf222aa9f93bd470242
tushark39 <tushark39@gmail.com>
2020-08-01 00:33:20 +0530

    added product

--- ecom/api/category/apps.py
+++ ecom/api/category/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class CategoryConfig(AppConfig):
     name = 'category'
--- ecom/api/product/views.py
+++ ecom/api/product/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import ProductSerializer
+from .models import Product
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
