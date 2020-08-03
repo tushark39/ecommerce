9a2ecea088717df9323cdee039d90d8029a22608
tushark39 <tushark39@gmail.com>
2020-08-04 00:31:13 +0530

    front_end_structure

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+   );
+  }
 }
 
 export default App;
